00cc0e481bf8ae6cabad895191d5a69219a91e16
Olivert75 <oliver.ton75@gmai.com>
2022-02-28 18:54:49 -0600

    Finished

--- extra_SQL_practie.sql
+++ extra_SQL_practie.sql
@@ -87,4 +87,124 @@ SELECT name
   FROM country
  WHERE population > 200000000;
 
+## 3.) Give the name and the per capita GDP for those countries with a population of at least 200 million.
+select name, gnp/population
+from country
+where population >= 200000000;
 
+/* 4.) Show the name and population in millions 
+for the countries of the continent 'South America'. Divide the population by 1000000 
+to get population in millions.
+*/
+select name, population/1000000
+from country
+where continent like 'South America' ;
+
+## 5.) Show the name and population for France, Germany, Italy
+select name, population
+from country
+where name in ('France', 'Germany', 'Italy');
+
+## 6.) Show the countries which have a name that includes the word 'United'
+select name 
+from country
+where name like '%United%';
+
+/* 7.) Two ways to be big: A country is big if it has an area of more than 3 million sq km 
+or it has a population of more than 250 million.
+Show the countries that are big by area or big by population. Show name, population and area. */
+select name, population, area
+from country
+where area > 3000000
+or population > 250000000;
+
+/* 8.) Exclusive OR (XOR). Show the countries that are big by area (more than 3 million) 
+or big by population (more than 250 million) but not both. Show name, population and area. */
+select name, population, area
+from country
+where (area > 3000000 or population > 250000000) and not (area > 3000000 and population > 250000000);
+
+/* 9.) Show the name and population in millions and the GDP in billions for the countries of the continent 'South America'. 
+Use the ROUND function to show the values to two decimal places. */
+select name,
+round(population/1000000,2),
+round(gdp/1000000000,2)
+from country
+where continent like 'South America';
+
+## if it's in the select, it makes its own column so you don't have to make them. 
+
+/* 10.) Show the name and per-capita GDP for those countries with
+ a GDP of at least one trillion (1000000000000; that is 12 zeros). 
+ Round this value (per capita gdp) to the nearest 1000. */
+select name,
+round (gdp/population,-3)
+from country 
+where gdp > 1000000000000;
+
+/* 11.) Show the name and capital where the name and the capital have the same number of characters. */
+SELECT name, capital
+  FROM world
+ WHERE len(name) = len(capital);
+
+ /* 12.) Show the name and the capital where the first letters of each match. 
+ Don't include countries where the name and the capital are the same word.
+You can use the function LEFT to isolate the first character.
+You can use <> as the NOT EQUALS operator. */
+select name, capital
+from world
+where left(name,1) = left(capital,1)
+and name <> capital;
+
+/* 13.) Equatorial Guinea and Dominican Republic have all of the vowels (a e i o u) in the name. 
+They don't count because they have more than one word in the name.
+Find the country that has all the vowels and no spaces in its name. */
+select name
+from world
+where name like '%a%'
+and name like '%e%'
+and name like '%i%'
+and name like '%o%'
+and name like '%u%'
+and name not like '% %'
+
+--Show the lastName, party and votes for the constituency 'S14000024' in 2017
+
+SELECT lastName, party, votes
+FROM ge
+ WHERE constituency = 'S14000024' AND yr = 2017
+ORDER BY votes DESC
+
+
+/*Show the party and RANK for constituency S14000024 in 2017. List the output by party
+You can use the RANK function to see the order of the candidates.
+ If you RANK using (ORDER BY votes DESC) then the candidate with the most votes has rank 1. */
+
+SELECT party, votes,
+RANK() OVER (ORDER BY votes DESC) as posn
+FROM ge
+WHERE constituency = 'S14000024' AND yr = 2017
+ORDER BY party
+
+
+/*The 2015 election is a different PARTITION to the 2017 election. 
+We only care about the order of votes for each year.
+Use PARTITION to show the ranking of each party in S14000021 in each year. 
+Include yr, party, votes and ranking (the party with the most votes is 1). */
+ 
+SELECT yr,party, votes,
+RANK() OVER (PARTITION BY yr ORDER BY votes DESC) as posn
+FROM ge
+WHERE constituency = 'S14000021'
+ORDER BY party,yr
+ 
+/*Edinburgh constituencies are numbered S14000021 to S14000026.
+Use PARTITION BY constituency to show the ranking of each party in Edinburgh in 2017. 
+Order your results so the winners are shown first, then ordered by constituency. */
+ 
+SELECT constituency,party, votes, 
+rank() over(partition by constituency order by votes desc) as rank
+FROM ge
+WHERE constituency BETWEEN 'S14000021' AND 'S14000026'
+AND yr  = 2017
+ORDER BY rank,constituency
